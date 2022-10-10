641e4bb36de01f58b51a55744ad9a1a1fc23b763
Jkawala <91265409+Jkawala@users.noreply.github.com>
2022-10-10 14:18:37 +0100

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts " Hello World"
+#The "puts" method prints out the variable
+puts  (" Enter your name")
+#asking the user to input a variable
+name = gets()
+#gets the user information which is the name
+puts ("Hello #{name}")
