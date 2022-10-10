6b558ee73048e3cbe452cf5e717c2e617ed68909
Jkawala <91265409+Jkawala@users.noreply.github.com>
2022-10-10 14:48:33 +0100

    Add files via upload

---
+++ numbers.rb
@@ -0,0 +1,35 @@
+#the math symbols include:
+#"+" plus
+#"-" minus
+#"/"slash or divide
+# "*"asterisk or multiply
+# "%"percent
+# "<" less
+#">" greater
+#"<=" less than equal
+#">=" greater than equal
+
+puts "I will count my farm animals:"
+
+puts "Sheep #{25 + 30 / 6}"
+# adding and divinding the number of sheep
+puts "Cows #{100 - 25 * 3 % 4}"
+#the cows are subtracted and multiplied into a percantage
+
+puts "Now I will count the goats:"
+
+puts 1 + 2 + 3 - 4 +5 % 2 - 1 / 4 + 6
+
+puts " Is it true that 3 + 2 < 5 -7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "How many times does 2 go into 20? #{20/2}"
+
+puts "oh, that's why it's false."
+
+puts "which one is greater 5 or -2? #{5 > -2}"
+puts "what about if its greater or equals? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+ 
